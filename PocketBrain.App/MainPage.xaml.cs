0bd5c03af34d2f22cd67632a55b6be6fbb178faf
bsautermeister <apps@bsautermeister.de>
2014-02-11 16:05:39 +0100

    Initial commit.

---
+++ PocketBrain.App/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PocketBrain.App.Resources;
+
+namespace PocketBrain.App
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Konstruktor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Beispielcode zur Lokalisierung der ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        // Beispielcode zur Erstellung einer lokalisierten ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // ApplicationBar der Seite einer neuen Instanz von ApplicationBar zuweisen
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Eine neue Schaltfläche erstellen und als Text die lokalisierte Zeichenfolge aus AppResources zuweisen.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Ein neues Menüelement mit der lokalisierten Zeichenfolge aus AppResources erstellen
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
