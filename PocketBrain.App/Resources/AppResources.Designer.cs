332fe3e292235765131b7619c301332e45b91aad
bsautermeister <apps@bsautermeister.de>
2014-06-21 18:11:05 +0200

    Changed version from 1.9 to 1.10

--- PocketBrain.App/Resources/AppResources.Designer.cs
+++ PocketBrain.App/Resources/AppResources.Designer.cs
@@ -97,7 +97,7 @@ namespace PocketBrain.App.Resources {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Zeichenfolge, die 1.9 ähnelt.
+        ///   Sucht eine lokalisierte Zeichenfolge, die 1.10 ähnelt.
         /// </summary>
         public static string ApplicationVersion {
             get {
